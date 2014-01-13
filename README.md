b55bd151b5a56e921719935e7bfc9788f594bded
Robbie Clutton <robert.clutton@gmail.com>
2014-01-12 22:36:04 -0500

    Init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Pi dev box
+
+## Dependencies
+
+- [VirtualBox](https://www.virtualbox.org)
+- [Vagrant](http://www.vagrantup.com/)
+
+## Setup
+
+### Get the code
+
+    git checkout git@github.com:barnyard/pi-dev-box.git
+    cd pi-dev-box
+
+### Start the box
+
+    vagrant up
+
+This should download a virtual machine and build it with the puppet manifest in `puppet/manifest/pi.pp`
---
+++ puppet/manifests/pi.pp
@@ -0,0 +1,24 @@
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin']
+}
+
+# --- Preinstall Stage ---------------------------------------------------------
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class apt_get_update {
+  exec { 'apt-get -y update':
+  }
+}
+
+class { 'apt_get_update':
+  stage => preinstall
+}
+
+# --- Developement dependencies -----------------------------------------------
+
+package { ['git', 'openjdk-7-jdk']:
+  ensure => installed
+}
