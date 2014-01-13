3422ddf8c8601ffd6e52a75c2a411c86d4ff8693
Robbie Clutton <robert.clutton@gmail.com>
2014-01-12 22:51:52 -0500

    Add Pi sources

--- puppet/manifests/pi.pp
+++ puppet/manifests/pi.pp
@@ -22,3 +22,38 @@ class { 'apt_get_update':
 package { ['git', 'openjdk-7-jdk', 'ant', 'ant-contrib']:
   ensure => installed
 }
+
+# --- Pi source ---------------------------------------------------------------
+
+exec { 'create workspace':
+  command => "mkdir -p /home/vagrant/workspace"
+}
+
+exec { 'clone p2p-build source':
+  command => "git clone https://github.com/barnyard/p2p-build.git",
+  cwd => '/home/vagrant/workspace',
+  creates => '/home/vagrant/workspace/p2p-build'
+}
+
+exec { 'clone freepastry source':
+  command => "git clone https://github.com/barnyard/freepastry.git",
+  cwd => '/home/vagrant/workspace',
+  creates => '/home/vagrant/workspace/freepastry'
+}
+
+exec { 'clone p2p-core source':
+  command => "git clone https://github.com/barnyard/p2p-core.git",
+  cwd => '/home/vagrant/workspace',
+  creates => '/home/vagrant/workspace/p2p-core'
+}
+
+exec { 'clone p2p-app source':
+  command => "git clone https://github.com/barnyard/p2p-app.git",
+  cwd => '/home/vagrant/workspace',
+  creates => '/home/vagrant/workspace/p2p-app'
+}
+
+exec { 'permissions':
+  command => "chown vagrant: -R /home/vagrant/workspace"
+}
+
